32df0de2c53292b03ead83976c6cd6fc19a58244
Bregy Malpartida <bregy@minsky.cc>
2025-08-07 16:55:07 -0500

    feat: refactor main.cpp to remove ESP-NOW functionality and implement cellular automata rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,123 +1,139 @@
-#include <Arduino.h>
+#include <ESP_8_BIT_composite.h>
 #include <WiFi.h>
-#include <esp_now.h>
 
+const uint8_t input_pot_1_pin = 34;
+const uint8_t input_pot_2_pin = 35;
+const uint8_t input_pot_3_pin = 32;
+const uint8_t input_pot_4_pin = 33;
 
-// REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+const int CELL_SIZE_X = 2;
+const int CELL_SIZE_Y = 2;
 
-// Structure example to send data
-// Must match the receiver structure
-typedef struct struct_message {
-  char a[32];
-  int b;
-  float c;
-  bool d;
-} struct_message;
+const int PIXELS_X = 256;
+const int PIXELS_Y = 240;
 
-// Create a struct_message called myData
-struct_message myData;
+const int CELL_LIFETIME = 7;
 
-esp_now_peer_info_t peerInfo;
+const int CELLS_X = PIXELS_X / CELL_SIZE_X;
+const int CELLS_Y = PIXELS_Y / CELL_SIZE_Y;
 
-// callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+const uint8_t STATE_DEAD = 0;
+const uint8_t STATE_ALIVE = CELL_LIFETIME - 1;
+
+ESP_8_BIT_composite video_out(true);
+
+uint16_t born_rule = 0b000001000;     // {3}
+uint16_t survive_rule = 0b000001100;  // {3,2}
+
+uint8_t board[(CELLS_Y * CELLS_X)];
+uint8_t board_copy[(CELLS_Y * CELLS_X)];
+
+void evolve();
+
+void render(uint8_t** frameBufferLines, int color_multiplier) {
+  for (int y = 0; y < PIXELS_Y; y++) {
+    for (int x = 0; x < PIXELS_X; x++) {
+      int index = (y / CELL_SIZE_Y) * CELLS_Y + (x / CELL_SIZE_X);
+
+      frameBufferLines[y][x] = board[index] * color_multiplier / CELL_LIFETIME;
+    }
+  }
+
+  video_out.waitForFrame();
 }
- 
+
+void generate_center_line(uint8_t thickness) {
+  int y_from = CELLS_Y / 2 - 1 * thickness;
+  int y_to = CELLS_Y / 2 + 1 * thickness;
+
+  for (int y = y_from; y < y_to; y++) {
+    for (int x = 0; x < CELLS_X; x++) {
+      board[y * CELLS_Y + x] = random(0, CELL_LIFETIME);
+    }
+  }
+}
+
 void setup() {
-  // Init Serial Monitor
-  Serial.begin(115200);
- 
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
+  video_out.begin();
 
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
+  Serial.begin(115200);
+
+  Serial.println("Connecting to WiFi");
+
+  for (int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
+    Serial.print(".");
+    delay(1000);
   }
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Transmitted packet
-  esp_now_register_send_cb(OnDataSent);
-  
-  // Register peer
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
-  peerInfo.encrypt = false;
-  
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
-    return;
+  analogReadResolution(9);
+
+  for (int y = 0; y < CELLS_Y; y++) {
+    for (int x = 0; x < CELLS_X; x++) {
+      board[y * CELLS_Y + x] = random(0, CELL_LIFETIME);
+    }
   }
 }
- 
+
 void loop() {
-  // Set values to send
-  strcpy(myData.a, "THIS IS A CHAR");
-  myData.b = random(1,20);
-  myData.c = 1.2;
-  myData.d = false;
-  
-  // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-   
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
-  }
-  else {
-    Serial.println("Error sending the data");
-  }
-  delay(2000);
-}
+  uint8_t** frame_buffer = video_out.getFrameBufferLines();
 
-// Structure example to receive data
-// Must match the sender structure
-typedef struct struct_message {
-  char a[32];
-  int b;
-  float c;
-  bool d;
-} struct_message;
+  int color_multiplier = map(analogRead(input_pot_1_pin), 0, 511, 0, 255);
 
-// Create a struct_message called myData
-struct_message myData;
+  born_rule = analogRead(input_pot_2_pin);
+  survive_rule = analogRead(input_pot_3_pin);
 
-// callback function that will be executed when data is received
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
-  memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("Char: ");
-  Serial.println(myData.a);
-  Serial.print("Int: ");
-  Serial.println(myData.b);
-  Serial.print("Float: ");
-  Serial.println(myData.c);
-  Serial.print("Bool: ");
-  Serial.println(myData.d);
-  Serial.println();
-}
+  int center_line_force = map(analogRead(input_pot_4_pin), 0, 511, 0, 15);
 
-void setup() {
-  // Initialize Serial Monitor
-  Serial.begin(115200);
+  render(frame_buffer, color_multiplier);
 
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
+  evolve();
 
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
+  if (center_line_force > 0) {
+    generate_center_line(center_line_force);
   }
 
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
-  esp_now_register_recv_cb(OnDataRecv);
+  int current_time = millis();
 }
 
-void loop() {}
+void evolve() {
+  memcpy(board_copy, board, sizeof(uint8_t) * CELLS_Y * CELLS_X);
+
+  for (int y = 0; y < CELLS_Y; y++) {
+    for (int x = 0; x < CELLS_X; x++) {
+      int current_state = board[y * CELLS_Y + x];
+
+      uint8_t total_n = 0;
+
+      for (int dx = -1; dx < 2; dx++) {
+        for (int dy = -1; dy < 2; dy++) {
+          if (dx != 0 || dy != 0) {
+            int neighborX = (x + dx) % CELLS_X;
+            int neighborY = (y + dy) % CELLS_Y;
+
+            int neighbor_state = board[neighborY * CELLS_Y + neighborX];
+
+            if (neighbor_state == STATE_ALIVE) {
+              total_n += 1;
+            }
+          }
+        }
+      }
+
+      if (current_state == STATE_DEAD) {
+        if ((born_rule >> total_n) & 1) {
+          board_copy[y * CELLS_Y + x] = STATE_ALIVE;
+        }
+      } else if (current_state == STATE_ALIVE) {
+        if (!((survive_rule >> total_n) & 1)) {
+          board_copy[y * CELLS_Y + x] = STATE_DEAD;
+        }
+      } else {
+        board_copy[y * CELLS_Y + x] = current_state - 1;
+      }
+    }
+  }
+
+  memcpy(board, board_copy, sizeof(uint8_t) * CELLS_Y * CELLS_X);
+
+  return;
+}
